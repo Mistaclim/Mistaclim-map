b5a8d15fddbecbba71d8af04df6045e9ae9e2176
Avi Shavit <avramshavit@gmail.com>
2024-10-19 23:32:38 +0300

    add basic leaflet map of the area

---
+++ main.js
@@ -0,0 +1,2 @@
+const map = L.map('map').setView([32, 35], 10); 
+const tiles = L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', { maxZoom: 19, attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>' }).addTo(map); 
